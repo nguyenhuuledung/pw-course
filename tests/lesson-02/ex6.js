622556e98dd5a69209605199c75e55e2609c292a
PW-Dung <ledung.nguyenhuu@gmail.com>
2025-11-26 20:43:23 +0700

    format code

--- tests/lesson-02/ex6.js
+++ tests/lesson-02/ex6.js
@@ -1,9 +1,9 @@
 
 let i = 1;
-for ( i = 1; i <= 100; i++){
-    if (i >=12 && i <=30){
-        console.log(`${i}` );
+for (i = 1; i <= 100; i++) {
+    if (i >= 12 && i <= 30) {
+        console.log(`${i}`);
     }
-    
+
 }
 
--- tests/lesson-02/ex9.js
+++ tests/lesson-02/ex9.js
@@ -1,8 +1,8 @@
 let i = 25;
-for ( i = 25; i >= 12; i--){
- 
-        console.log(`${i}` );
+for (i = 25; i >= 12; i--) {
 
-    
+    console.log(`${i}`);
+
+
 }
 
